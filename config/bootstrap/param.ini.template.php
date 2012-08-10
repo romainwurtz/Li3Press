d85184ad73368a8a62b74e9817874b8eeea8e7e2
Romain Wurtz <t3kila@Romain-Wurtzs-MacBook-Pro-2.local>
2012-08-10 15:14:58 -0700

    Template ready

---
+++ config/bootstrap/param.ini.template.php
@@ -0,0 +1,24 @@
+<?php
+ /**
+  * Li3Press: A simple blog using Lithium framework
+  *
+  * @author 		Romain Wurtz (http://www.t3kila.com)
+  * @copyright		Copyright 2012, Romain Wurtz (http://www.t3kila.com)
+  * 
+  */
+
+/* Lithium core libraries */
+define('LITHIUM_LIBRARY_PATH', '');		// Ex: /home/[NAME]/Sites/lithium/libraries/ 
+    
+/* Connection to the db */
+define('DB_HOST' , '');					// Ex: localhost 
+define('DB_LOGIN' , '');				// Ex: root 
+define('DB_PASSWORD' , '');				// Ex: root 
+define('DB_DATABASE' , '');				// Ex: li3press
+define('DB_ADAPTER' , 'MySql');			// Mysql is the only one supported
+
+/* Website config */
+define('BLOG_TITLE' , '');				// Ex: My amazing blog
+define('BLOG_BG' , '');					// Ex: /img/bg.jpg
+
+?>
