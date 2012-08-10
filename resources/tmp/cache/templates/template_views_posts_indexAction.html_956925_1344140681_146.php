69351057e4d667bef9a4ff5b9f7a75dc90c3e54f
Romain Wurtz <t3kila@Romain-Wurtzs-MacBook-Pro-2.local>
2012-08-10 15:07:38 -0700

    Git exclude temp

--- resources/tmp/cache/templates/template_views_posts_indexAction.html_956925_1344140681_146.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($posts as $post): ?>
-<article>
-    <h1><?php echo $h($post->title); ?></h1>
-    <p><?php echo $post->body ?></p>
-</article>
-<?php endforeach; ?>
--- resources/tmp/cache/templates/template_views_sessions_add.html_958618_1344194744_637.php
+++
@@ -1,9 +0,0 @@
-<?php echo $this->form->create(null, array("id" => "form_login", "class" => "form-vertical")); ?>
- <fieldset>
-    <?php echo $this->form->field('username', array('id' => 'login_username', 'autocomplete' => 'on', 'class' => 'span3', 'wrap' => array('class' => 'control-group'))); ?>
-    <?php echo $this->form->field('password', array('type' => 'password', 'autocomplete' => 'off', 'class' => 'span3', 'id' => 'login_pass', 'wrap' => array('class' => 'control-group'))); ?>
-        <div style="margin-top:42px">
-    <?php echo $this->form->submit('Login', array('class' => 'btn btn-success btn-large span3')); ?>
-        </div>
-     </fieldset>
-<?php echo $this->form->end(); ?>
