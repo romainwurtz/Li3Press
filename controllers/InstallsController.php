1ffaaeb832d1946961f0a51577e99a161a03af85
Adrien Candiotti <adrien.candiotti@gmail.com>
2012-08-17 16:40:14 -0700

    mod - clean InstallsController.php

--- controllers/InstallsController.php
+++ controllers/InstallsController.php
@@ -19,6 +19,34 @@ class InstallsController extends \lithium\action\Controller {
 
 	protected static $_install = false;
 
+	# FIXME : put into a file
+	protected static $_sql = "
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE IF NOT EXISTS `posts` (`id` int(11) NOT NULL AUTO_INCREMENT,
+				    `title` text NOT NULL,
+				    `body` text NOT NULL,
+				    `visibility` tinyint NOT NULL DEFAULT 1,
+				     PRIMARY KEY (`id`)) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE IF NOT EXISTS `users` (`id` int(11) NOT NULL AUTO_INCREMENT,
+				    `username` varchar(256) NOT NULL,
+				    `password` varchar(256) NOT NULL,
+				    PRIMARY KEY (`id`), UNIQUE KEY `username` (`username`)) ENGINE=InnoDB;";
+	# ENDFIXME
+
+	# FIXME : put into the unittest mock, replace with some instructions 
+	# about the blog maybe?
+	protected static $_lorem = "Cupcake ipsum dolor sit. Amet pastry cheesecake. Danish sesame snaps caramels wypas. Caramels candy dragée dragée halvah cupcake. Bear claw gingerbread tiramisu candy tart sweet roll marzipan. Icing macaroon faworki.\
+Halvah chocolate macaroon bonbon jelly macaroon faworki. Cheesecake dessert chocolate cake cotton candy sesame snaps fruitcake.<br />Marshmallow dragée chocolate cake tiramisu candy muffin sugar plum sesame snaps. Sugar plum halvah ice cream wafer toffee icing. Chocolate bar candy donut topping gummies tiramisu muffin pudding. Gummies oat cake marzipan pie ice cream marshmallow macaroon marzipan caramels.\
+Pie biscuit pudding jelly beans chocolate oat cake. Danish cotton candy tootsie roll. Jelly muffin lemon drops wafer bonbon wafer chocolate bar dessert. Pie danish tootsie roll cotton candy. Halvah chocolate pie dragée apple pie candy canes marshmallow. Liquorice bonbon apple pie jelly-o jelly-o.\
+Biscuit bonbon powder pie pastry cheesecake gummies. Tootsie roll jelly marzipan pastry tootsie roll chupa chups chupa chups wafer.<br />Gummies marshmallow sugar plum apple pie marzipan cookie.\ Cupcake gummies pastry. Chupa chups lollipop croissant. Tart lollipop carrot cake applicake.<br />Gummies caramels cheesecake liquorice dragée. Chocolate bar sesame snaps chupa chups candy canes. Soufflé cookie chupa chups cheesecake pudding cookie.\
+Toffee cheesecake cupcake caramels jujubes gingerbread cookie sesame snaps. Candy canes caramels cupcake cotton candy oat cake. Cheesecake cheesecake dessert tart. Donut tiramisu apple pie. Sweet roll pudding donut chocolate tiramisu tiramisu marzipan. Chocolate cake sesame snaps sweet.";
+	# ENDFIXME
+
 	protected function _init() {
 		parent::_init();
 
@@ -39,37 +67,16 @@ class InstallsController extends \lithium\action\Controller {
 		$success = false;
 		$errors = array();
 		if (!self::$_install && $this -> request -> data) {
-			$sql = "DROP TABLE IF EXISTS `posts`;
-					CREATE TABLE IF NOT EXISTS `posts` (
-					`id` int(11) NOT NULL AUTO_INCREMENT,
-					`title` text NOT NULL,
-					`body` text NOT NULL,
-					`visibility` TINYINT DEFAULT 1 NOT NULL,
-					PRIMARY KEY (`id`)
-					) ENGINE=InnoDB;
-					DROP TABLE IF EXISTS `users`;
-					CREATE TABLE IF NOT EXISTS `users` (
-					`id` int(11) NOT NULL AUTO_INCREMENT,
-					`username` varchar(256) NOT NULL,
-					`password` varchar(256) NOT NULL,
-					PRIMARY KEY (`id`),
-					UNIQUE KEY `username` (`username`)
-					) ENGINE=InnoDB;";
-			Model::connection() -> read($sql);
-			if (!($success = UsersController::addUser($this -> request -> data['user'], $this -> request -> data['password'], $errors))) {
-				$errors['User'] = "User can't be created.";
-			}
-			if ($success && !($success = PostsController::addPost("Post example", "Cupcake ipsum dolor sit. Amet pastry cheesecake. Danish sesame snaps caramels wypas. Caramels candy dragée dragée halvah cupcake. Bear claw gingerbread tiramisu candy tart sweet roll marzipan. Icing macaroon faworki.
-Halvah chocolate macaroon bonbon jelly macaroon faworki. Cheesecake dessert chocolate cake cotton candy sesame snaps fruitcake.<br />Marshmallow dragée chocolate cake tiramisu candy muffin sugar plum sesame snaps. Sugar plum halvah ice cream wafer toffee icing. Chocolate bar candy donut topping gummies tiramisu muffin pudding. Gummies oat cake marzipan pie ice cream marshmallow macaroon marzipan caramels.
-Pie biscuit pudding jelly beans chocolate oat cake. Danish cotton candy tootsie roll. Jelly muffin lemon drops wafer bonbon wafer chocolate bar dessert. Pie danish tootsie roll cotton candy. Halvah chocolate pie dragée apple pie candy canes marshmallow. Liquorice bonbon apple pie jelly-o jelly-o.
-Biscuit bonbon powder pie pastry cheesecake gummies. Tootsie roll jelly marzipan pastry tootsie roll chupa chups chupa chups wafer.<br />Gummies marshmallow sugar plum apple pie marzipan cookie. Cupcake gummies pastry. Chupa chups lollipop croissant. Tart lollipop carrot cake applicake.<br />Gummies caramels cheesecake liquorice dragée. Chocolate bar sesame snaps chupa chups candy canes. Soufflé cookie chupa chups cheesecake pudding cookie.
-Toffee cheesecake cupcake caramels jujubes gingerbread cookie sesame snaps. Candy canes caramels cupcake cotton candy oat cake. Cheesecake cheesecake dessert tart. Donut tiramisu apple pie. Sweet roll pudding donut chocolate tiramisu tiramisu marzipan. Chocolate cake sesame snaps sweet.", $errors))) {
+		  Model::connection() -> read(self::$_sql);
+		  if (!($success = UsersController::addUser($this -> request -> data['user'], $this -> request -> data['password'], $errors))) {
+		    $errors['User'] = "User can't be created.";
+		  }
+		  if ($success && !($success = PostsController::addPost("Post example", self::$_lorem, $errors))) {
 				$errors['Post'] = "Post can't be created.";
-			}
-			if ($success) {
-				self::$_install = true;
-			}
-
+		  }
+		  if ($success) {
+		    self::$_install = true;
+		  }
 		}
 		return compact('success', 'errors');
 	}
--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -15,20 +15,6 @@ use lithium\action\DispatchException;
 
 class UsersController extends \lithium\action\Controller {
 
-	protected function getUsers(&$users) {
-		$users = Users::find('all');
-		return count($users);
-	}
-
-	protected function getUser($id, &$user) {
-		$user = null;
-		$success = false;
-
-		if ($id > 0)
-			$success = (($user = Users::find($id))) ? true : false;
-		return $success;
-	}
-
 	public function addUser($login, $password, &$errors) {
 		$success = false;
 
@@ -54,9 +40,9 @@ class UsersController extends \lithium\action\Controller {
 		} else if (!$this -> request -> is('post')) {
 			$errors['call'] = 'This action can only be called with post';
 		} else if ($this -> request -> data) {
-			$user = Users::create($this -> request -> data);
-			if (!($success = $user -> save())) {
-				$errors = $user -> errors();
+			$post = Users::create($this -> request -> data);
+			if (!($success = $post -> save())) {
+				$errors = $post -> errors();
 			}
 		}
 		return compact('success', 'errors');
@@ -66,78 +52,6 @@ class UsersController extends \lithium\action\Controller {
 		if (!Auth::check('default')) {
 			return $this -> redirect('Sessions::add');
 		}
-	}
-
-	public function listUsers() {
-		$users = array();
-
-		if (!Auth::check('default')) {
-			return $this -> redirect('Sessions::add');
-		}
-		self::getUsers($users);
-		return compact('users');
-	}
-
-	public function deleteAction() {
-		$url = "";
-		$success = false;
-		$errors = array();
-
-		if (!Auth::check('default')) {
-			$errors['login'] = 'You need to be logged.';
-		} else if (!$this -> request -> is('post')) {
-			$errors['call'] = 'This action can only be called with post';
-		} else {
-			if (!($success = self::getUser($this -> request -> data['id'], $user))) {
-				$errors['user'] = 'This user doesn\'t exist';
-			} else if (!($success = $user -> delete())) {
-				$errors = $post -> errors();
-			}
-		}
-		return compact('success', 'errors');
-	}
-
-	public function view() {
-		$success = self::getUser($this -> request -> id, $user);
-		return compact('success', 'user');
-	}
-
-	public function edit() {
-		if (!Auth::check('default')) {
-			return $this -> redirect('Sessions::add');
-		}
-		if (!($success = self::getUser($this -> request -> id, $user))) {
-			$this -> redirect(array('Users::add'));
-		}
-		return compact('success', 'user');
-	}
-
-	public function editAction() {
-		$errors = array();
-		$success = false;
-		$errors = array();
-
-		if (!Auth::check('default')) {
-			$errors['login'] = 'You need to be logged.';
-		} else if (!$this -> request -> is('post')) {
-			$errors['call'] = 'This action can only be called with post';
-		} else {
-			if (!($success = self::getUser($this -> request -> data['id'], $user))) {
-				$errors['user'] = 'This post doesn\'t exist';
-			}
-			if ($success && $this -> request -> data) {
-				if (!($success = $user -> save($this -> request -> data))) {
-					$errors += $user -> errors();
-				} else {
-					$currentSession = Auth::check('default');
-					if ($currentSession && $currentSession['id'] == $this -> request -> data['id']) {
-						$currentSession = $this -> request -> data + $currentSession;
-						Auth::set('default', $currentSession);
-					}
-				}
-			}
-		}
-		return compact('success', 'errors');
 	}
 
 }
