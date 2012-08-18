40795f119c550ac10f34228dc2c627e29165b3d9
Adrien Candiotti <adrien.candiotti@gmail.com>
2012-08-17 17:27:57 -0700

    mod - hide bug corrected, database flaw corrected

--- views/posts/edit.html.php
+++ views/posts/edit.html.php
@@ -4,8 +4,15 @@
     	<?=$this->form->field('body', array('type' => 'textarea', 'autocomplete' => 'off', 'class' => 'span9', 'style' => 'height:200px', 'id' => 'edit_body', 'wrap' => array('class' => 'control-group')));?>
     <div style="margin-top:42px">
     	<?=$this->form->submit('Edit Post', array('class' => 'btn btn-success btn-large span2 right')); ?>
-	<?php $text = 'Hide Post'; if (!$post->visibility) $text = 'Show Post';
-	  echo $this->html->link($text, '', array('class' => 'btn btn-large btn-info span2 right', 'id' => 'hide_post')); ?> 
+<?php
+	  $text = 'Show Post';
+	  $class = 'btn btn-large btn-info span2 right';
+	  if ($post->visibility) {
+	    $text = 'Hide Post';
+	    $class .= ' visible';
+	  }
+  
+	  echo $this->html->link($text, '', array('class' => $class, 'id' => 'hide_post')); ?> 
     	<?php echo $this->html->link('Delete post', $this->url(array('Posts::editAction', 'id' => $this->request()->id)), array('class' => "btn btn-large btn-danger span2 right", 'id' => 'delete_post')); ?> 
     </div>
  </fieldset>
@@ -35,7 +42,6 @@
             },
             success: function (data) {
                 if (data) {
-                    console.log(data);
                     if (data.success) {
                         $('#content').prepend('<div class="alert alert-block alert-success fade in">\
 						<button type="button" class="close" data-dismiss="alert">&times;</button>\
@@ -91,7 +97,6 @@
             success: function (data) {
                 if (data) {
 		  if (data.success) {
-		      console.log($(button).hasClass('visible'));
 		      if ($(button).hasClass('visible')) {
 			$(button).removeClass('visible');
 			$(button).html('Show Post');
@@ -106,7 +111,7 @@
                 }
             },
             error: function (XMLHttpRequest, textStatus, errorThrown) {}
-        });
+	});
 	        });
 });
 </script>
